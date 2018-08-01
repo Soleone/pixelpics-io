2ba7e223dc78d88801d4098c720bbd0ff04aeab6
Dennis Theisen <dennis@shopify.com>
2018-07-31 21:18:56 -0400

    Revert "Try to fix asset public path"
    This reverts commit ca0a29823c67826ac30b9c0df70c0e5569fc7c22.
    
    Trying again and again

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/pixelpics-io/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
