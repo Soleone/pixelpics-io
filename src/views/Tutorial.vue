529f263c24e33e0aecb01e57436c4ddb0da3456d
Dennis Theisen <dennis@shopify.com>
2018-08-17 03:48:40 -0400

    Improve tutorial copy

--- src/views/Tutorial.vue
+++ src/views/Tutorial.vue
@@ -5,7 +5,10 @@
     </b-row>
     <b-row>
       <b-col align-h="center" class="text-center mt-3">
-        <b-alert show variant="success">{{ carouselTitle }}</b-alert>
+        <b-alert show variant="success">
+          <h6>{{ carouselTitle }}</h6>
+          <div>{{ carouselSubTitle }}</div>
+        </b-alert>
       </b-col>
     </b-row>
     <b-row align-h="center">
@@ -40,7 +43,7 @@
     </b-row>
     <b-row>
       <b-col align-h="center" class="text-center mt-3">
-        <b-btn variant="danger" :to="{name: 'boards', params: {id: 1 }}">Skip tutorial</b-btn>
+        <b-btn v-show="!isLastSlide " variant="danger" :to="{name: 'boards', params: {id: 1 }}">Skip tutorial</b-btn>
       </b-col>
     </b-row>
   </b-container>
@@ -49,12 +52,30 @@
 <script>
 
 const CAROUSEL_TITLES = [
-  'Solve the picture using the hints.',
-  'Select the correct tiles.',
-  'Mark tiles you don\'t want to select.',
-  'Multiple numbers in a row represent tilesets with empty spaces in between.',
-  'To win select all correct tiles.',
-  'Learn strategies as you play.'
+  [
+    'Uncover the secret picture.',
+    'The numbers are hints on how many tiles need to be selected.'
+  ],
+  [
+    'Select the correct tiles.',
+    'Click on tiles to select them if you think they are correct.'
+  ],
+  [
+    'Mark tiles you don\'t want to select.',
+    'Once you ruled out that a tile needs to be selected you can mark it in a different color.'
+  ],
+  [
+    'Focus on the connected tiles.',
+    'The numbers represent sets of connected tiles with empty spaces in between.'
+  ],
+  [
+    'To win select all correct tiles.',
+    'Early on focus on the edges of the board and larger sets of numbers.'
+  ],
+  [
+    'Learn strategies as you play.',
+    'Once you get the hang of it you can even create your own puzzles for other people.'
+  ]
 ]
 
 export default {
@@ -67,10 +88,16 @@ export default {
   },
   computed: {
     carouselTitle () {
-      return CAROUSEL_TITLES[this.slide]
+      return CAROUSEL_TITLES[this.slide][0]
+    },
+    carouselSubTitle () {
+      return CAROUSEL_TITLES[this.slide][1]
     },
     background () {
-      return this.slide === CAROUSEL_TITLES.length - 1 ? '#fefefe' : '#d4d4d4'
+      return this.isLastSlide ? '#fefefe' : '#d4d4d4'
+    },
+    isLastSlide () {
+      return this.slide === CAROUSEL_TITLES.length - 1
     }
   }
 }
